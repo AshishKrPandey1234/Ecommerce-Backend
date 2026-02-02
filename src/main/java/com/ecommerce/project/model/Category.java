0fa08924b819f1e88fe8d741bf60b06e94f1a583
Ashish Kumar Pandey <akpandey369936@gmail.com>
2026-02-03 00:25:25 +0530

    Added Category APIs with controller-service-repository flow and ResponseEntity handling

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.model;
+
+public class Category {
+    private Long CategoryId;
+    private String CategoryName;
+
+    public Category(Long categoryId, String categoryName) {
+        CategoryId = categoryId;
+        CategoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+}
